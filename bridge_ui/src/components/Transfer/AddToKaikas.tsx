231472bdccaad0b0a55445a54842de7a5cc7d858
Sotatek-DuyNguyen2 <duy.nguyen2@sotatek.com>
2022-01-27 15:56:13 +0700

    feat: add token to kaikas

---
+++ bridge_ui/src/components/Transfer/AddToKaikas.tsx
@@ -0,0 +1,83 @@
+import { CHAIN_ID_KLAYTN_BAOBAB } from "@certusone/wormhole-sdk";
+import { Button, makeStyles } from "@material-ui/core";
+import { useCallback } from "react";
+import { useSelector } from "react-redux";
+import { useKaikasProvider } from "../../contexts/KaikasProviderContext";
+import {
+  selectTransferSourceParsedTokenAccount,
+  selectTransferTargetAsset,
+  selectTransferTargetChain,
+} from "../../store/selectors";
+import { getEvmChainId } from "../../utils/consts";
+import { klaytnTokenToParsedTokenAccount } from "../../utils/klaytn";
+
+const useStyles = makeStyles((theme) => ({
+  addButton: {
+    display: "block",
+    margin: `${theme.spacing(1)}px auto 0px`,
+  },
+}));
+
+export default function AddToKaikas() {
+  const classes = useStyles();
+  const sourceParsedTokenAccount = useSelector(
+    selectTransferSourceParsedTokenAccount
+  );
+  const targetChain = useSelector(selectTransferTargetChain);
+  const targetAsset = useSelector(selectTransferTargetAsset);
+  
+  const {provider: providerKaikas, signerAddress: signerAddressKaikas, chainId: chainIdKaikas} = useKaikasProvider();
+  const hasCorrectEvmNetwork = chainIdKaikas === getEvmChainId(targetChain);
+  const handleClick = useCallback(() => {
+    if (providerKaikas && targetAsset && signerAddressKaikas && hasCorrectEvmNetwork) {
+      (async () => {
+        try {
+            
+            const result = await klaytnTokenToParsedTokenAccount(targetAsset, providerKaikas, signerAddressKaikas);
+            console.log(result);
+            
+            const {decimals, symbol, address} = result;
+            const {klaytn} = window;
+            klaytn.sendAsync(
+                {
+                    method: 'wallet_watchAsset',
+                    params: {
+                      type: 'ERC20', 
+                      options: {
+                        address, 
+                        symbol: symbol ||  sourceParsedTokenAccount?.symbol, 
+                        decimals,
+                        image: "" 
+                      }
+                    },
+                    id: Math.round(Math.random() * 100000)
+                  },
+                  (err: any, result: any) => console.log(err, result)
+            )
+        } catch (e) {
+          console.error(e);
+        }
+      })();
+    }
+  }, [
+    targetAsset,
+    hasCorrectEvmNetwork,
+    providerKaikas,
+    signerAddressKaikas,
+    sourceParsedTokenAccount
+  ]);
+  return providerKaikas &&
+    signerAddressKaikas &&
+    targetAsset &&
+    targetChain === CHAIN_ID_KLAYTN_BAOBAB &&
+    hasCorrectEvmNetwork ? (
+    <Button
+      onClick={handleClick}
+      size="small"
+      variant="outlined"
+      className={classes.addButton}
+    >
+      Add to Kaikas
+    </Button>
+  ) : null;
+}
