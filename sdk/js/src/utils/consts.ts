98a7a5b36c80f9d2d10cce16ff3033babab273e7
Sotatek-DuyNguyen2 <duy.nguyen2@sotatek.com>
2022-02-18 16:49:19 +0700

    fix: fix chain id of baobab

--- sdk/js/src/utils/consts.ts
+++ sdk/js/src/utils/consts.ts
@@ -1,4 +1,4 @@
-export type ChainId = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 13 |  1001 | 4097 | 10001;
+export type ChainId = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 19 |  1001 | 4097 | 10001;
 export const CHAIN_ID_SOLANA: ChainId = 1;
 export const CHAIN_ID_ETH: ChainId = 2;
 export const CHAIN_ID_TERRA: ChainId = 3;
@@ -9,7 +9,7 @@ export const CHAIN_ID_OASIS: ChainId = 7;
 export const CHAIN_ID_ALGORAND: ChainId = 8;
 export const CHAIN_ID_AURORA: ChainId = 1001;
 export const CHAIN_ID_FANTOM: ChainId = 10;
-export const CHAIN_ID_KLAYTN_BAOBAB: ChainId = 13;
+export const CHAIN_ID_KLAYTN_BAOBAB: ChainId = 19;
 export const CHAIN_ID_ETHEREUM_ROPSTEN: ChainId = 6;
 
 export const WSOL_ADDRESS = "So11111111111111111111111111111111111111112";
